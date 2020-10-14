259468e1e0692d56bf167618353d71282dfc2fef
Jessamine Brown <brownjessa11@gmail.com>
2020-10-14 09:42:44 -0500

    fix script source and errors

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-$ = (queryString) => document.querySelector(queryString)
+$ = (queryString) => document.querySelector(queryString);
 // function to set new color 60 times a second
 const shifthue = (hue) => (hue + 1) % 360;
 
-let hue =0;
+let hue = 0;
 
 // function to pass callback for animation
-const animate() => {
-    hue = shiftHue(hue); 
-    const color = 'hsl(${hue}, 100%, 50%)';
-    // make sphere blue for 60 seconds
-    $('a-sphere').settAttribute('color', color);
+const animate = () => {
+  hue = shiftHue(hue);
+  const color = "hsl(${hue}, 100%, 50%)";
+  $("a-sphere").settAttribute("color", color);
+  requestAnimationFrame(animate);
 };
 // create animation
 requestAnimationFrame(animate);
