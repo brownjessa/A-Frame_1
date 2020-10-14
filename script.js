4e8d28cdbfadff242d895cecde310d7c86d38395
Jessamine Brown <brownjessa11@gmail.com>
2020-10-14 09:33:37 -0500

    flow color

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 $ = (queryString) => document.querySelector(queryString)
+// function to set new color 60 times a second
+const shifthue = (hue) => (hue + 1) % 360;
+
+let hue =0;
+
 // function to pass callback for animation
 const animate() => {
-
+    hue = shiftHue(hue); 
+    const color = 'hsl(${hue}, 100%, 50%)';
     // make sphere blue for 60 seconds
-    $('a-sphere').settAttribute('color', 'blue');
+    $('a-sphere').settAttribute('color', color);
 };
 // create animation
 requestAnimationFrame(animate);
